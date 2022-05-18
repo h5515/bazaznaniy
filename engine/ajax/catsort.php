8daccdd489d01a4ebee412d2dce5a6a4fbfc15b7
Aleks <h5515@mail.ru>
2022-05-18 09:13:16 +0600

    bz

--- engine/ajax/catsort.php
+++ engine/ajax/catsort.php
@@ -1,13 +1,13 @@
 <?php
 @session_start();
-@error_reporting( E_ALL ^ E_WARNING ^ E_NOTICE );
-@ini_set( 'display_errors', true );
-@ini_set( 'html_errors', false );
-@ini_set( 'error_reporting', E_ALL ^ E_WARNING ^ E_NOTICE );
+@error_reporting(E_ALL ^ E_WARNING ^ E_NOTICE);
+@ini_set('display_errors', true);
+@ini_set('html_errors', false);
+@ini_set('error_reporting', E_ALL ^ E_WARNING ^ E_NOTICE);
 
-define( 'DATALIFEENGINE', true );
-define( 'ROOT_DIR', '../..' );
-define( 'ENGINE_DIR', '..' );
+define('DATALIFEENGINE', true);
+define('ROOT_DIR', '../..');
+define('ENGINE_DIR', '..');
 
 include ENGINE_DIR . '/data/config.php';
 
@@ -16,234 +16,234 @@ require_once ENGINE_DIR . '/classes/templates.class.php';
 require_once ENGINE_DIR . '/data/dbconfig.php';
 require_once ENGINE_DIR . '/modules/functions.php';
 require_once ENGINE_DIR . '/modules/sitelogin.php';
-require_once ROOT_DIR . '/language/' . $config[ 'langs' ] . '/website.lng';
-require_once( DLEPlugins::Check( ENGINE_DIR . '/modules/functions.php' ) );
+require_once ROOT_DIR . '/language/' . $config['langs'] . '/website.lng';
+require_once(DLEPlugins::Check(ENGINE_DIR . '/modules/functions.php'));
 
-function listdir($dir) {
-	
-	$current_dir = @opendir( $dir );
-	
-	if($current_dir !== false ) {
-		while ( $entryname = readdir( $current_dir ) ) {
-			if( is_dir( $dir."/".$entryname ) AND ($entryname != "." AND $entryname != "..") ) {
-				listdir( $dir."/".$entryname );
-			} elseif( $entryname != "." AND $entryname != ".." ) {
-				@unlink( $dir."/".$entryname );
+function listdir($dir)
+{
+
+	$current_dir = @opendir($dir);
+
+	if ($current_dir !== false) {
+		while ($entryname = readdir($current_dir)) {
+			if (is_dir($dir . "/" . $entryname) and ($entryname != "." and $entryname != "..")) {
+				listdir($dir . "/" . $entryname);
+			} elseif ($entryname != "." and $entryname != "..") {
+				@unlink($dir . "/" . $entryname);
 			}
 		}
-		@closedir( $current_dir );
-		@rmdir( $dir );
+		@closedir($current_dir);
+		@rmdir($dir);
 	}
-
 }
 
-function clear_all_caches() {
+function clear_all_caches()
+{
 	global $config;
-	
-	listdir( ENGINE_DIR . '/cache/system/CSS' );
-	listdir( ENGINE_DIR . '/cache/system/HTML' );
-	listdir( ENGINE_DIR . '/cache/system/URI' );
-	listdir( ENGINE_DIR . '/cache/system/plugins' );
+
+	listdir(ENGINE_DIR . '/cache/system/CSS');
+	listdir(ENGINE_DIR . '/cache/system/HTML');
+	listdir(ENGINE_DIR . '/cache/system/URI');
+	listdir(ENGINE_DIR . '/cache/system/plugins');
 
 	$catalog = '';
 	$catl = '';
 	if (isset($_COOKIE['dbname'])) {
 		$catalog = $_COOKIE['dbname'];
-		$catl = $_COOKIE['dbname'].'/';
+		$catl = $_COOKIE['dbname'] . '/';
 	}
-	
-	$fdir = opendir( ENGINE_DIR . '/cache/system/'.$catalog );
-	while ( $file = readdir( $fdir ) ) {
-		if( $file != '.' AND $file != '..' AND $file != '.htaccess' AND $file != 'cron.php' ) {
-			@unlink( ENGINE_DIR . '/cache/system/'.$catl . $file );
-		
+
+	$fdir = opendir(ENGINE_DIR . '/cache/system/' . $catalog);
+	while ($file = readdir($fdir)) {
+		if ($file != '.' and $file != '..' and $file != '.htaccess' and $file != 'cron.php') {
+			@unlink(ENGINE_DIR . '/cache/system/' . $catl . $file);
 		}
 	}
-	
-	if( $config['cache_type'] ) {
-		$fdir = opendir( ENGINE_DIR . '/cache'.$catl );
-		while ( $file = readdir( $fdir ) ) {
-			if( $file != '.htaccess' AND !is_dir($file) ) {
-					@unlink( ENGINE_DIR . '/cache/'.$catl . $file );
+
+	if ($config['cache_type']) {
+		$fdir = opendir(ENGINE_DIR . '/cache' . $catl);
+		while ($file = readdir($fdir)) {
+			if ($file != '.htaccess' and !is_dir($file)) {
+				@unlink(ENGINE_DIR . '/cache/' . $catl . $file);
 			}
 		}
 	}
-	
+
 	clear_cache();
-	
+
 	if (function_exists('opcache_reset')) {
 		opcache_reset();
 	}
-	
 }
 
 function parseJsonArray($jsonArray, $parentID = 0)
 {
-  $return = array();
-  foreach ($jsonArray as $subArray) {
-     $returnSubSubArray = array();
-     if (isset($subArray['children'])) {
-       $returnSubSubArray = parseJsonArray($subArray['children'], $subArray['id']);
-     }
-     $return[] = array('id' => $subArray['id'], 'parentid' => $parentID);
-     $return = array_merge($return, $returnSubSubArray);
-  }
+	$return = array();
+	foreach ($jsonArray as $subArray) {
+		$returnSubSubArray = array();
+		if (isset($subArray['children'])) {
+			$returnSubSubArray = parseJsonArray($subArray['children'], $subArray['id']);
+		}
+		$return[] = array('id' => $subArray['id'], 'parentid' => $parentID);
+		$return = array_merge($return, $returnSubSubArray);
+	}
 
-  return $return;
+	return $return;
 }
-$glcat = $_REQUEST['id'];
-$glcat = get_idcategories( $glcat );
+if (isset($_REQUEST['idcategory']))
+	$glcat = $_REQUEST['idcategory'];
+else
+	$glcat = $_REQUEST['id'];
+$glcat = get_idcategories($glcat);
 
-    $glcat = ( int )$glcat;
+$glcat = (int)$glcat;
 
 if ($_REQUEST['action'] == "vostan") {
-		if( !$user_group[$member_id[ 'user_group' ] ][ 'moderation' ] ) die ("error");	
-	if( $_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash ) {		
-		die ("error");
-	}	
-	$id = ( int )$_POST['id'];
-	if ($id==0) die ("error");
-	$cots = getSubCatList( $id );
-        $aviable = array();
-        $aviable = explode( ',', $cots );
-	$sql = "UPDATE dle_post set arhiv = 0 WHERE category regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
+	if (!$user_group[$member_id['user_group']]['moderation']) die("error");
+	if ($_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash) {
+		die("error");
+	}
+	$id = (int)$_POST['id'];
+	if ($id == 0) die("error");
+	$cots = getSubCatList($id);
+	$aviable = array();
+	$aviable = explode(',', $cots);
+	$sql = "UPDATE dle_post set arhiv = 0 WHERE category regexp '[[:<:]](" . implode('|', $aviable) . ")[[:>:]]'";
 	$db->query($sql);
-	
-	$sql = "UPDATE dle_category set arhiv = 0 WHERE id regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
-	$db->query($sql);	
-	
+
+	$sql = "UPDATE dle_category set arhiv = 0 WHERE id regexp '[[:<:]](" . implode('|', $aviable) . ")[[:>:]]'";
+	$db->query($sql);
+
 	$buffer = 'reload';
 }
 
 if ($_REQUEST['action'] == "update") {
-		if( !$user_group[$member_id[ 'user_group' ] ][ 'moderation' ] ) die ("error");	
-	if( $_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash ) {		
-		die ("error");
-	}	
-	$id = ( int )$_POST['id'];
-	if ($id==0) die ("error");
-	
+	if (!$user_group[$member_id['user_group']]['moderation']) die("error");
+	if ($_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash) {
+		die("error");
+	}
+	$id = (int)$_POST['id'];
+	if ($id == 0) die("error");
+
 	$catname = $_POST['name'];
-	
-	$alt_cat_name = totranslit( stripslashes( $catname ), true, false );
-	
+
+	$alt_cat_name = totranslit(stripslashes($catname), true, false);
+
 	$row = $db->super_query("SELECT COUNT(id) as c FROM dle_category WHERE alt_name='{$alt_cat_name}'");
 
-	if ($row['c']!=0){
+	if ($row['c'] != 0) {
 		$alt_cat_name .= random_int(1, 100000);
 	}
-	
+
 	$sql = "UPDATE dle_category set name = '{$catname}', alt_name = '{$alt_cat_name}' WHERE id = {$id}";
-	$db->query($sql);		
-	
+	$db->query($sql);
+
 	$buffer = $alt_cat_name;
-	clear_all_caches();	
+	clear_all_caches();
 }
 
 if ($_REQUEST['action'] == "del") {
-		if( !$user_group[$member_id[ 'user_group' ] ][ 'moderation' ] ) die ("error");	
-	if( $_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash ) {		
-		die ("error");
-	}	
-	$id = ( int )$_POST['id'];
-	if ($id==0) die ("error");
-	$cots = getSubCatList( $id );
-        $aviable = array();
-        $aviable = explode( ',', $cots );
+	if (!$user_group[$member_id['user_group']]['moderation']) die("error");
+	if ($_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash) {
+		die("error");
+	}
+	$id = (int)$_POST['id'];
+	if ($id == 0) die("error");
+	$cots = getSubCatList($id);
+	$aviable = array();
+	$aviable = explode(',', $cots);
 	//$sql = "DELETE dle_post set arhiv = 1 WHERE category regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
 	//$db->query($sql);
-	
-	$sql = "DELETE FROM dle_category WHERE id regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
-	$db->query($sql);	
-	
-	$sql = "SELECT id FROM dle_post Where category regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
+
+	$sql = "DELETE FROM dle_category WHERE id regexp '[[:<:]](" . implode('|', $aviable) . ")[[:>:]]'";
+	$db->query($sql);
+
+	$sql = "SELECT id FROM dle_post Where category regexp '[[:<:]](" . implode('|', $aviable) . ")[[:>:]]'";
 	$result = $db->query($sql);
-	
-	while ( $row = $db->get_row( $sql_result ) ) {
-		deletenewsbyid( $row['id'], false);
+
+	while ($row = $db->get_row($sql_result)) {
+		deletenewsbyid($row['id'], false);
 	}
-	clear_all_caches();	
+	clear_all_caches();
 	$buffer = 'reloadparent';
 }
 
 if ($_REQUEST['action'] == "arhiv") {
-		if( !$user_group[$member_id[ 'user_group' ] ][ 'moderation' ] ) die ("error");	
-	if( $_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash ) {		
-		die ("error");
-	}	
-	$id = ( int )$_POST['id'];
-	if ($id==0) die ("error");
-	$cots = getSubCatList( $id );
-        $aviable = array();
-        $aviable = explode( ',', $cots );
-	$sql = "UPDATE dle_post set arhiv = 1 WHERE category regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
+	if (!$user_group[$member_id['user_group']]['moderation']) die("error");
+	if ($_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash) {
+		die("error");
+	}
+	$id = (int)$_POST['id'];
+	if ($id == 0) die("error");
+	$cots = getSubCatList($id);
+	$aviable = array();
+	$aviable = explode(',', $cots);
+	$sql = "UPDATE dle_post set arhiv = 1 WHERE category regexp '[[:<:]](" . implode('|', $aviable) . ")[[:>:]]'";
 	$db->query($sql);
-	
-	$sql = "UPDATE dle_category set arhiv = 1 WHERE id regexp '[[:<:]](" . implode( '|', $aviable ) . ")[[:>:]]'";
-	$db->query($sql);	
-	
+
+	$sql = "UPDATE dle_category set arhiv = 1 WHERE id regexp '[[:<:]](" . implode('|', $aviable) . ")[[:>:]]'";
+	$db->query($sql);
+
 	$buffer = 'reloadparent';
 }
 
 if ($_REQUEST['action'] == "addcat") {
-	if( !$user_group[$member_id[ 'user_group' ] ][ 'moderation' ] ) die ("error");	
-	if( $_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash ) {		
-		die ("error");
+	if (!$user_group[$member_id['user_group']]['moderation']) die("error");
+	if ($_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash) {
+		die("error");
 	}
 	$catname = $_POST['name'];
-	$parentid = ( int )$_POST['id'];
-	$alt_cat_name = totranslit( stripslashes( $catname ), true, false );
-	
-	if ($parentid==0) die ("error");
-	if ($catname=="") die ("error");	
-	
+	$parentid = (int)$_POST['id'];
+	$alt_cat_name = totranslit(stripslashes($catname), true, false);
+
+	if ($parentid == 0) die("error");
+	if ($catname == "") die("error");
+
 	$row = $db->super_query("SELECT COUNT(id) as c FROM dle_category WHERE alt_name='{$alt_cat_name}'");
 
-	if ($row['c']!=0){
+	if ($row['c'] != 0) {
 		$alt_cat_name .= random_int(1, 100000);
 	}
 
 	$row = $db->super_query("SELECT MAX(posi) as max FROM dle_category");
-	$pos = $row['max']+1;
-	
-	$db->query( "INSERT INTO dle_category (parentid, name, posi, alt_name) VALUES ({$parentid},'{$catname}',{$pos},'{$alt_cat_name}')");
-	
+	$pos = $row['max'] + 1;
+
+	$db->query("INSERT INTO dle_category (parentid, name, posi, alt_name) VALUES ({$parentid},'{$catname}',{$pos},'{$alt_cat_name}')");
+
 	$id = $db->insert_id();
-	
-	$buffer = $id."|".$alt_cat_name;
+
+	$buffer = $id . "|" . $alt_cat_name;
 	clear_all_caches();
-	
 }
 
 
 if ($_REQUEST['action'] == "catsort") {
 
-	if( !$user_group[$member_id[ 'user_group' ] ][ 'moderation' ] ) die ("error");	
-	if( $_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash ) {		
-		die ("error");
+	if (!$user_group[$member_id['user_group']]['moderation']) die("error");
+	if ($_REQUEST['user_hash'] == "" or $_REQUEST['user_hash'] != $dle_login_hash) {
+		die("error");
 	}
-	
+
 	$_POST['list'] = json_decode(stripslashes($_POST['list']), true);
 
-	if ( !is_array($_POST['list']) ) die ("error");
-	
-	$_POST['list'] = parseJsonArray($_POST['list']);
-	
-	$i= 0;
+	if (!is_array($_POST['list'])) die("error");
 
-	foreach ( $_POST['list'] as $value ) {
+	$_POST['list'] = parseJsonArray($_POST['list']);
+
+	$i = 0;
+
+	foreach ($_POST['list'] as $value) {
 		$i++;
 
 		$id = intval($value['id']);
 		$parentid = intval($value['parentid']);
-		
-		if ( $id ) {
-			//$buffer .= $id.", ";
-			if ($parentid!=0) $parent = "parentid='{$parentid}',";
-				else
-					$parent = "parentid='{$glcat}',";
-			$db->query( "UPDATE " . PREFIX . "_category SET $parent posi='{$i}' WHERE id = '{$id}'" );
 
+		if ($id) {
+			//$buffer .= $id.", ";
+			if ($parentid != 0) $parent = "parentid='{$parentid}',";
+			else
+				$parent = "parentid='{$glcat}',";
+			$db->query("UPDATE " . PREFIX . "_category SET $parent posi='{$i}' WHERE id = '{$id}'");
 		}
 	}
 
@@ -256,8 +256,4 @@ if ($_REQUEST['action'] == "catsort") {
 }
 
 
-	echo $buffer;
-
-
-
-?>
+echo $buffer;
--- templates/Default/js/new/bz_menu.js
+++ templates/Default/js/new/bz_menu.js
@@ -1,57 +1,58 @@
 htmlm = `
 <ul id='menu' style='display:none;'>
   <li><span class='popmenu polosniz' elem='menu_del'></span></li>
+  <li><span class='popmenu' elem='dostup'> Доступ </span></li>
   <li><span class='popmenu' elem='edit_name'> Редактировать имя </span></li>
   <li><span class='popmenu' elem='edit_obl'> Изменить обложку </span></li>
   <li><span class='popmenu' elem='arhive'> Архивировать </span></li>
   <li><span class='popmenu' elem='delete'> Удалить </span></li>
 </ul>
-`;
+`
 
-$('#dle-content .dle-center').append(htmlm);
-var menus;
-menus = $("#menu").kendoContextMenu({
-    target: ".edit_btn a",
-    //filter: ".product",
+$('#dle-content .dle-center').append(htmlm)
+var menus
+menus = $('#menu').kendoContextMenu({
+    target: '.edit_btn a',
+    // filter: ".product",
     animation: {
-        open: { effects: "fadeIn" },
+        open: { effects: 'fadeIn' },
         duration: 200
     },
-    showOn: "click",
+    showOn: 'click',
     open: openmenu,
     select: selectmenu,
     close: function(e) {
-        menus.element.find("[elem=noarhive]").text(' Архивировать ').attr('elem', 'arhive');
+        menus.element.find('[elem=noarhive]').text(' Архивировать ').attr('elem', 'arhive')
     }
-}).data("kendoContextMenu");
+}).data('kendoContextMenu')
 
 function openmenu(e) {
-    $elem = $(e.target);
-    namebz = $elem.closest('ol').find('.shottitle').text();
-    // menus.enable(e.items[0], $(e.items[0]).hasClass("k-disabled"));
-    var bg = $elem.closest('ol').find('.circleimg').css('background-image');
-    bg = bg.replace('url(', '').replace(')', '').replace(/\"/gi, "");
-    urls = $elem.closest('ol').find('.menuurl').attr('href');
-    menus.remove(menus.element.children("li:first"));
+    $elem = $(e.target)
+    namebz = $elem.closest('ol').find('.shottitle').text()
+        // menus.enable(e.items[0], $(e.items[0]).hasClass("k-disabled"))
+    var bg = $elem.closest('ol').find('.circleimg').css('background-image')
+    bg = bg.replace('url(', '').replace(')', '').replace(/\"/gi, '')
+    urls = $elem.closest('ol').find('.menuurl').attr('href')
+    menus.remove(menus.element.children('li:first'))
     menus.insertBefore({
         text: namebz,
         imageUrl: bg,
-        url: urls,
-    }, menus.element.children("li:first"));
-    if ($elem.closest('ol').find(".bzarhive")[0]) {
-        menus.element.find("[elem=arhive]").text(' Восстановить ').attr('elem', 'noarhive');
+        url: urls
+    }, menus.element.children('li:first'))
+    if ($elem.closest('ol').find('.bzarhive')[0]) {
+        menus.element.find('[elem=arhive]').text(' Восстановить ').attr('elem', 'noarhive')
     }
 }
 
 function selectmenu(e) {
-    $elem = $(e.target);
-    namebz = $elem.closest('ol').find('.shottitle').text();
-    poper = $(e.item).find('.popmenu').attr('elem');
-    catid = $elem.closest('ol').attr('cat');
-    idcategory = $elem.closest('ol').attr('idcat');
-    project = '';
+    $elem = $(e.target)
+    namebz = $elem.closest('ol').find('.shottitle').text()
+    poper = $(e.item).find('.popmenu').attr('elem')
+    catid = $elem.closest('ol').attr('cat')
+    idcategory = $elem.closest('ol').attr('idcat')
+    project = ''
     if (catid == 2) {
-        project = $elem.closest('ol').attr('project');
+        project = $elem.closest('ol').attr('project')
     }
     if (poper == 'edit_name') {
         kendo.prompt('Новое имя:', namebz).then(function(data) {
@@ -63,14 +64,43 @@ function selectmenu(e) {
                 project: project,
                 namebd: rus_to_latin(data)
             }
-            getajax('/php/add_bz.php', dt);
-        });
-        $('.k-prompt .k-window-title').text('Изменить имя базы знаний: ' + namebz);
-        $('.k-prompt').css('width', '450px');
+            getajax('/php/add_bz.php', dt)
+        })
+        $('.k-prompt .k-window-title').text('Изменить имя базы знаний: ' + namebz)
+        $('.k-prompt').css('width', '450px')
+    }
+    if (poper == 'dostup') {
+        if (!$('#iddostup')[0]) {
+            $('#addbz').after('<div id = "iddostup" > < /div>')
+        }
+        windostup = $('#iddostup').kendoWindow({
+            width: '600px',
+            title: 'Настройки доступа для ' + namebz,
+            content: '/tpl/dostup_bz.html',
+            visible: false,
+            modal: true,
+            pinned: false,
+            resizable: false,
+            autoFocus: true,
+            open: function(e) {
+                $('html, body').css('overflow', 'hidden')
+                this.center()
+            },
+            close: function(e) {
+                $('html, body').css('overflow', '');
+                setTimeout(() => {
+                    $('#iddostup').remove();
+                }, 400);
+            }
+        }).data('kendoWindow')
+        setTimeout(() => {
+            windostup.open()
+        }, 50);
+
     }
     if (poper == 'edit_obl') {
         if (!$('#ideditobloz')[0]) {
-            $("#addbz").after('<div id = "ideditobloz" > < /div>')
+            $('#addbz').after('<div id = "ideditobloz" > < /div>')
         }
         editobloz = $('#ideditobloz').kendoWindow({
             width: '500px',
@@ -86,22 +116,22 @@ function selectmenu(e) {
             //     left: "35%",
             // },
             open: function(e) {
-                $("html, body").css('overflow', 'hidden');
+                $('html, body').css('overflow', 'hidden')
 
                 if (project != '') {
-                    project = "[project=" + project + "]";
+                    project = '[project=' + project + ']'
                 }
 
-                $("[cat=" + catid + "][idcat=" + idcategory + "]" + project).clone().appendTo("#id_edit_obloz .dle-center");
-                $("#id_edit_obloz .dle-center .edit_btn").remove();
-                $("#id_edit_obloz .dle-center a").attr('href', '#').attr('onclick', 'return false;');
-                this.center();
+                $('[cat=' + catid + '][idcat=' + idcategory + ']' + project).clone().appendTo('#id_edit_obloz .dle-center')
+                $('#id_edit_obloz .dle-center .edit_btn').remove()
+                $('#id_edit_obloz .dle-center a').attr('href', '#').attr('onclick', 'return false;')
+                this.center()
             },
             close: function(e) {
-                $("html, body").css('overflow', '');
+                $('html, body').css('overflow', '')
                 setTimeout(() => {
-                    $("#id_edit_obloz .dle-center").html('');
-                }, 400);
+                    $('#id_edit_obloz .dle-center').html('')
+                }, 400)
             }
         }).data('kendoWindow')
         setTimeout(() => {
@@ -109,39 +139,42 @@ function selectmenu(e) {
         }, 50)
     }
     if (poper == 'arhive') {
-        kendo.confirm("Архивировать базу знаний: <b>" + namebz + "?</b>").then(function() {
+        kendo.confirm('Архивировать базу знаний: <b>' + namebz + '?</b>').then(function() {
             dt = {
                 rezim: 'arhive',
                 category: catid,
                 idcategory: idcategory,
-                project: project,
+                project: project
             }
-            getajax('/php/add_bz.php', dt);
-        });
-        $('.k-window-title').text('Архивировать?');
+            getajax('/php/add_bz.php', dt)
+        })
+        $('.k-window-title').text('Архивировать?')
     }
     if (poper == 'noarhive') {
-        kendo.confirm("Восстановить базу знаний: <b>" + namebz + "?</b>").then(function() {
+        kendo.confirm('Восстановить базу знаний: <b>' + namebz + '?</b>').then(function() {
             dt = {
                 rezim: 'noarhive',
                 category: catid,
                 idcategory: idcategory,
-                project: project,
+                project: project
             }
-            getajax('/php/add_bz.php', dt);
-        });
-        $('.k-window-title').text('Архивировать?');
+            getajax('/php/add_bz.php', dt)
+        })
+        $('.k-window-title').text('Архивировать?')
     }
     if (poper == 'delete') {
-        kendo.confirm("Точно удалить базу знаний: <b>" + namebz + "?</b> Все статьи будут удалены.").then(function() {
-            dt = {
-                rezim: 'delete',
-                category: catid,
-                idcategory: idcategory,
-                project: project,
-            }
-            getajax('/php/add_bz.php', dt);
-        });
-        $('.k-window-title').text('Удалить?');
+        kendo.confirm('Точно удалить базу знаний: <b>' + namebz + '?</b> Все статьи будут удалены.').then(function() {
+            kendo.confirm('Действительно удалить базу знаний: <b>' + namebz + '?</b> будет удалено всё что связано с ней.').then(function() {
+                dt = {
+                    rezim: 'delete',
+                    user_hash: dle_login_hash,
+                    category: catid,
+                    idcategory: idcategory,
+                    project: project
+                }
+                getajax(dle_root + 'engine/ajax/controller.php?mod=catsort', dt)
+            })
+        })
+        $('.k-window-title').text('Удалить?')
     }
 }
