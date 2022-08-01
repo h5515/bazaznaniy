9a51b70f37dc54eae5f88f5ba76f593c6bfbc194
Aleks <h5515@mail.ru>
2022-08-01 18:52:35 +0600

    ия

--- engine/data/dopconfig.php
+++ engine/data/dopconfig.php
@@ -1,5 +1,5 @@
 <?php
-echo "2<br>";
+echo ENGINE_DIR."   - 2<br>";
 if (is_file(ENGINE_DIR . '/data/dopconfig/'.$dopconfigFile)){
     echo "3<br>";
     include ENGINE_DIR . '/data/dopconfig/'.$dopconfigFile;
